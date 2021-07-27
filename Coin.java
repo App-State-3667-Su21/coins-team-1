7af14353d679f427e399bbf7c51ef435b1c447bf
Kevin Kamto <kamtokevin@gmail.com>
2021-07-27 12:17:51 -0400

    fix: remove unnecessary import + rename variable

--- Coin.java
+++ Coin.java
@@ -1,14 +1,12 @@
-import jdk.nashorn.internal.ir.Symbol;
-
 // All coins have some common elements: a value, an identification of the country, motto, metallic composition, year of minting, etc. We are, for now, only concerned about value and country code. And, of course, there are different types of coins for the different countries as specified in the tables below. (Notice that the common names of the coins don't overlap! Except for US and Canadian quarter, dime, nickel. So we will keep the common name for USD and call the Canadian ones CadQuarter, etc.)
 abstract class Coin {
-    private String symbol;
+    private String cc;
     private String commonName;
     private double denomination;
 
     public String countryCode()
     {
-        return symbol;
+        return cc;
     }
     public String commonName()
     {
