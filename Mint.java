d683bff772ba0142410117ce4961f27dca7cd323
Noah Bodin <bodinnb@appstate.edu>
2021-07-27 12:18:58 -0400

    created mint abstraction

---
+++ Mint.java
@@ -0,0 +1,12 @@
+abstract class Mint {
+    public abstract Coin makeCoin(double value);
+
+    abstract void smelt();
+
+    abstract void imprint();
+
+    abstract void polish();
+
+    abstract void inspect();
+
+}
